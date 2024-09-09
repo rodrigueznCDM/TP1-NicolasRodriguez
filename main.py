14b0f7ace893a883084b4ea3a4a19bbb67c16033
rodrigueznCDM <rodriguezn@educ.college-montreal.qc.ca>
2024-09-09 14:51:09 -0400

    trouver bug prochaine période

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 print("TP1 - Compteur de Mots\n")
+
 chaine = str(input("Entrer la chaîne de caractères dont vous voulez compter les mots:\n"))
+chaine = chaine.strip()
+
+mots = 1
+next = 0
+
+for car in chaine:
+    if next == 1:
+        if car.isspace():
+            mots = mots + 1
+            next = next - 1
+            print("mot")
+        else:
+            print("pas mot")
+            next = next - 1
+
+    elif car.isalnum():
+        print("mot?")
+        next = next + 1
+
+
+print("\nIl y a", mots,"mot(s) dans cette chaîne de caractères")
